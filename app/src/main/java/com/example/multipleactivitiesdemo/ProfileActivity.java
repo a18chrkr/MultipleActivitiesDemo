50d7defd24c1c6a0dea34d66e8bbdca6990f90c9
a18chrkr <a18chrkr@gmail.com>
2022-04-12 15:29:00 +0200

    Skapan en onClick()-hanterare på logga-ut-knappen som tar användaren tillbaka till MainActivity. Ett meddelande skrivs även ut i Logcat.

--- app/src/main/java/com/example/multipleactivitiesdemo/ProfileActivity.java
+++ app/src/main/java/com/example/multipleactivitiesdemo/ProfileActivity.java
@@ -2,10 +2,15 @@ package com.example.multipleactivitiesdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button signout;
 
     @Override
     protected void onStart() {
@@ -29,5 +34,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        signout = findViewById(R.id.signout_button);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("==>", "Du har loggat ut!");
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
